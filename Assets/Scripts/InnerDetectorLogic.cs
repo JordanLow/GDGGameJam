1b64726343978c57f11660b54c2bba9e63980368
Jordan Low <jordanlow@r-163-98-25-172.comp.nus.edu.sg>
2024-09-14 20:51:02 +0800

    fix

--- Assets/Scripts/InnerDetectorLogic.cs
+++ Assets/Scripts/InnerDetectorLogic.cs
@@ -1,26 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class InnerDetectorLogic : MonoBehaviour
 {
     public bool touchingPaper;
+    private int numCol;
     void Start()
     {
+        numCol = 0;
         touchingPaper = false;
     }
 
     void Update()
     {
-        touchingPaper = false;
+        touchingPaper = numCol > 0;
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Paper"))
         {
             Debug.Log("touching" +gameObject.name);
-            touchingPaper = true;
+            numCol++;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Paper"))
+        {
+            Debug.Log("touching" +gameObject.name);
+            numCol++;
         }
     }
 }
