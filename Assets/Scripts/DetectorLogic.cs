9e041c0b513d1bb3268ec560a7bf534afb49dfaf
Jordan Low <jordanlow.jinyi@gmail.com>
2024-09-15 14:25:10 +0800

    fixed aspect ratio

--- Assets/Scripts/DetectorLogic.cs
+++ Assets/Scripts/DetectorLogic.cs
@@ -22,10 +22,10 @@ public class DetectorLogic : MonoBehaviour
     }
 	
 	void Update() {
-		timer.text = string.Format("{0:N1}", timeLeft);
 		if (timeLeft == -999f) {
 			return;
 		}
+		timer.text = string.Format("{0:N1}", timeLeft);
 		if (timeLeft <= 0) {
 			timer.text = "0.0";
 			timeLeft = -999f;
